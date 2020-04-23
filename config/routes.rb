cf4e5312457e367e131dd33279278b4e274b3d96
Terrence Bowman <tbowman.oh@gmail.com>
2020-04-23 17:02:53 -0600

    One

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
+  post '/answers' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
 end
